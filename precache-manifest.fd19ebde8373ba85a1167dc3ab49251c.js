ffa5d977f6658480c610a228513ab7029206014d
RafaMelara98 <00182516@uca.edu.sv>
2019-11-01 11:31:36 -0600

    Updates

---
+++ precache-manifest.fd19ebde8373ba85a1167dc3ab49251c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffae6d68bbe825b4bc384e729f3fac55",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "9ed4ac3ce889e77a1af0",
+    "url": "/TicTacToe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/TicTacToe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "9ed4ac3ce889e77a1af0",
+    "url": "/TicTacToe/static/js/main.cf8ab3cb.chunk.js"
+  },
+  {
+    "revision": "664d87082df4062f2db9",
+    "url": "/TicTacToe/static/js/runtime-main.326ff5c5.js"
+  }
+]);
